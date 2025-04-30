e07146ddf41c3600c8fa9196b67a20aa7ec2898e
QLxie <2351834712@qq.com>
2025-04-30 13:00:22 +0800

    "发布界面"

--- miniprogram/pages/publish/publish.js
+++ miniprogram/pages/publish/publish.js
@@ -1,39 +1,124 @@
 // pages/publish/publish.js
 Page({
 
-  data: {
-
+    /**
+     * 页面的初始数据
+     */
+    data: {
+      value: '',
+      //图片
+        fileList: [{
+          url: 'https://img.yzcdn.cn/vant/leaf.jpg',//这是一个固定的URL
+          name: '图片1',
+          isImage: true,
+          deletable: true,
+        },
+        // Uploader 根据文件后缀来判断是否为图片文件
+        // 如果图片 URL 中不包含类型信息，可以添加 isImage 标记来声明
+      ],
+  
+      //日历数据(暂未启用)
+      date: '',
+      show: false,
+    },
+  
+   //图片上传函数
+   afterRead(event) {
+      const { file } = event.detail;
+      // 当设置 mutiple 为 true 时, file 为数组格式，否则为对象格式
+      wx.uploadFile({
+        url: 'https://example.weixin.qq.com/upload', // 仅为示例，非真实的接口地址
+        filePath: file.url,
+        name: 'file',
+        formData: { user: 'test' },
+        success(res) {
+          // 上传完成需要更新 fileList
+          const { fileList = [] } = this.data;
+          fileList.push({ ...file, url: res.data });
+          this.setData({ fileList });
+        },
+      });
   },
-
-  onLoad(options) {
-
-  },
-
-  onReady() {
-
-  },
-
-  onShow() {
-
-  },
-
-  onHide() {
-
-  },
-
-  onUnload() {
-
-  },
-
-  onPullDownRefresh() {
-
-  },
-
-  onReachBottom() {
-
-  },
-
-  onShareAppMessage() {
-
-  }
-})
+  
+  
+  onChange(event) {
+      // event.detail 为当前输入的值
+      console.log(event.detail);
+    },
+  
+  
+  //日历函数（暂未启用)
+  onDisplay() {
+      this.setData({ show: true });
+    },
+    onClose() {
+      this.setData({ show: false });
+    },
+    formatDate(date) {
+      date = new Date(date);
+      return `${date.getMonth() + 1}/${date.getDate()}`;
+    },
+    onConfirm(event) {
+      this.setData({
+        show: false,
+        date: this.formatDate(event.detail),
+      });
+    },
+  
+  
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad(options) {
+  
+    },
+  
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady() {
+  
+    },
+  
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow() {
+  
+    },
+  
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide() {
+  
+    },
+  
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload() {
+  
+    },
+  
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh() {
+  
+    },
+  
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom() {
+  
+    },
+  
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage() {
+  
+    }
+  })
